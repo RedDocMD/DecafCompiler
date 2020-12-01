e44bfedea2e5e7a9ab126fc25ae99c50b54a03d7
Deep Majumder <deep.majumder2019@gmail.com>
2020-12-01 15:27:12 +0530

    First commit, lexer ready

---
+++ src/main/kotlin/deep/decaf/LexerTester.kt
@@ -0,0 +1,38 @@
+package deep.decaf
+
+import deep.decaf.lexer.*
+import org.antlr.v4.runtime.*
+import org.antlr.v4.runtime.misc.*
+import java.io.*
+
+fun main(args: Array<String>) {
+    val exampleDirName = "/home/deep/work/compiler/provided1/scanner"
+    val exampleDir = File(exampleDirName)
+    val fileNames = exampleDir.listFiles()?.filterNot { it.isDirectory }?.map { it.absolutePath }?.sorted()
+    if (fileNames != null) {
+        for (fileName in fileNames) {
+            println("\n$fileName")
+            val lexer = DecafLexer(CharStreams.fromFileName(fileName))
+            lexer.removeErrorListeners()
+            lexer.addErrorListener(object : BaseErrorListener() {
+                override fun syntaxError(
+                    recognizer: Recognizer<*, *>?,
+                    offendingSymbol: Any?,
+                    line: Int,
+                    charPositionInLine: Int,
+                    msg: String?,
+                    e: RecognitionException?
+                ) {
+                    val errMsg = "line $line:$charPositionInLine $msg"
+                    println(errMsg)
+                }
+            })
+            var token: Token
+            token = lexer.nextToken()
+            while (token.type != Token.EOF) {
+                println("${token.line} ${lexer.vocabulary.getSymbolicName(token.type)}  ${token.text}")
+                token = lexer.nextToken()
+            }
+        }
+    }
+}
