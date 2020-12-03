9b68325dfec7802a34e3e3ff47f26e599a67e63e
Deep Majumder <deep.majumder2019@gmail.com>
2020-12-03 16:07:59 +0530

    Added type map and some more type checking rules

--- src/main/kotlin/deep/decaf/analysis/TypeCheck.kt
+++ src/main/kotlin/deep/decaf/analysis/TypeCheck.kt
@@ -10,6 +10,71 @@ enum class Type {
 }
 
 class TypeAssigner : DecafBaseVisitor<Type>() {
+    private val idTypeMap = mutableMapOf<String, Type>()
+    private val methodArgListMap = mutableMapOf<String, List<Type>>()
+    private val methodReturnTypeMap = mutableMapOf<String, Type>()
+
+    override fun visitProgram(ctx: DecafParser.ProgramContext?): Type {
+        return if (ctx != null) {
+            val fieldDecls = ctx.field_decl()
+            val methodDecls = ctx.method_decl()
+            var nonErrorCount = 0
+            for (fieldDecl in fieldDecls) {
+                if (visit(fieldDecl) != Type.ERROR) {
+                   nonErrorCount++
+                }
+            }
+            for (methodDecl in methodDecls) {
+                if (visit(methodDecl) != Type.ERROR) {
+                    nonErrorCount++
+                }
+            }
+            if (nonErrorCount == fieldDecls.size + methodDecls.size) {
+                Type.VOID
+            } else {
+                Type.ERROR
+            }
+        } else Type.ERROR
+    }
+
+    override fun visitField_decl(ctx: DecafParser.Field_declContext?): Type {
+        return if (ctx != null) {
+            val type = visit(ctx.type())
+            val fieldNames = ctx.field_name()
+            for (fieldName in fieldNames) {
+                idTypeMap[fieldName.ID().text] = type
+            }
+            type
+        } else Type.ERROR
+    }
+
+    override fun visitType(ctx: DecafParser.TypeContext?): Type {
+        return if (ctx != null) {
+            if (ctx.TK_BOOLEAN() != null) Type.BOOL else Type.INT
+        } else Type.ERROR
+    }
+
+    override fun visitMethod_decl(ctx: DecafParser.Method_declContext?): Type {
+        return if (ctx != null) {
+            val returnType = if (ctx.TK_VOID() != null) Type.VOID else visit(ctx.type())
+            val methodName = ctx.ID().text
+            val args = ctx.arglist()?.arg()
+            if (args != null) {
+                val argTypeList = args.map { visit(it) }
+                methodArgListMap[methodName] = argTypeList
+            } else {
+                methodArgListMap[methodName] = listOf()
+            }
+            methodReturnTypeMap[methodName] = returnType
+            returnType
+        } else Type.ERROR
+    }
+
+    override fun visitArg(ctx: DecafParser.ArgContext?): Type {
+        return if (ctx != null) visit(ctx.type())
+        else Type.ERROR
+    }
+
     // Expression visitors start
     override fun visitNegExpr(ctx: DecafParser.NegExprContext?): Type {
         return if (ctx != null) {
