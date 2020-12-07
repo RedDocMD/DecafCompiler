b6f03aa0ee26c5da00f64fc6556c927200cbd37d
Deep Majumder <deep.majumder2019@gmail.com>
2020-12-07 10:22:07 +0530

    Added semantic checker to compiler cli

--- src/main/kotlin/deep/decaf/main/Compiler.kt
+++ src/main/kotlin/deep/decaf/main/Compiler.kt
@@ -1,5 +1,6 @@
 package deep.decaf.main
 
+import deep.decaf.ir.*
 import deep.decaf.parser.*
 import org.antlr.v4.runtime.*
 import picocli.*
@@ -17,11 +18,22 @@ class Compiler : Callable<Int> {
     @Parameters(index = "0", description = ["The file to compile"])
     lateinit var file: File
 
-    @Option(names = ["-s", "--scan"], description = ["Only scan for tokens"])
-    var scan = false
+    @ArgGroup(exclusive = true, multiplicity = "1")
+    lateinit var stage: Stage
 
-    @Option(names = ["-p", "--parse"], description = ["Only scan and then parse"])
-    var parse = false
+    class Stage {
+        @Option(names = ["-s", "--scan"], description = ["Only scan for tokens"])
+        var scan = false
+
+        @Option(names = ["-p", "--parse"], description = ["Only scan and then parse"])
+        var parse = false
+
+        @Option(names = ["-c", "--semantic"], description = ["Scan, parse and then perform semantic checks"])
+        var check = false
+    }
+
+    @Option(names = ["-g", "--debug"], description = ["Enable debug mode"])
+    var debug = false
 
     override fun call(): Int {
         if (!file.exists()) {
@@ -29,18 +41,37 @@ class Compiler : Callable<Int> {
             println(msg)
             return 1
         }
-        when {
-            scan -> return scanFile(file)
-            parse -> return parseFile(file)
+        return when {
+            stage.scan -> scanFile(makeLexer(file), debug)
+            stage.parse -> {
+                val lexer = makeLexer(file)
+                val exitCode = scanFile(lexer, debug)
+                if (exitCode == 0) {
+                    val parser = makeParser(lexer)
+                    val (newExitCode, _) = parseFile(parser)
+                    newExitCode
+                } else exitCode
+            }
+            stage.check -> {
+                val lexer = makeLexer(file)
+                val exitCode = scanFile(lexer, debug)
+                if (exitCode == 0) {
+                    val parser = makeParser(lexer)
+                    val (newExitCode, tree) = parseFile(parser)
+                    if (newExitCode == 0) {
+                        val ir = makeIR(tree)
+                        semanticCheck(ir, debug)
+                    } else newExitCode
+                } else exitCode
+            }
+            else -> 0
         }
-        return 0
     }
 }
 
 fun main(args: Array<String>): Unit = exitProcess(CommandLine(Compiler()).execute(*args))
 
-fun scanFile(file: File): Int {
-    val lexer = makeLexer(file)
+fun scanFile(lexer: DecafLexer, debug: Boolean): Int {
     var hasErrors = false
     lexer.removeErrorListeners()
     lexer.addErrorListener(object : BaseErrorListener() {
@@ -53,15 +84,17 @@ fun scanFile(file: File): Int {
             e: RecognitionException?
         ) {
             hasErrors = true
-            val errMsg = "line $line:$charPositionInLine $msg"
+            val errMsg = "$line:$charPositionInLine $msg"
             println(errMsg)
         }
     })
-    var token: Token
-    token = lexer.nextToken()
-    while (token.type != Token.EOF) {
-        println("${token.line} ${lexer.vocabulary.getSymbolicName(token.type)}  ${token.text}")
+    if (debug) {
+        var token: Token
         token = lexer.nextToken()
+        while (token.type != Token.EOF) {
+            println("${token.line} ${lexer.vocabulary.getSymbolicName(token.type)}  ${token.text}")
+            token = lexer.nextToken()
+        }
     }
     return if (hasErrors) 1 else 0
 }
@@ -70,9 +103,7 @@ fun makeLexer(file: File): DecafLexer = DecafLexer(CharStreams.fromFileName(file
 
 fun makeParser(lexer: DecafLexer): DecafParser = DecafParser(CommonTokenStream(lexer))
 
-fun parseFile(file: File): Int {
-    val lexer = makeLexer(file)
-    val parser = makeParser(lexer)
+fun parseFile(parser: DecafParser): Pair<Int, DecafParser.ProgramContext> {
     var hasErrors = false
     parser.removeErrorListeners()
     parser.addErrorListener(object : BaseErrorListener() {
@@ -85,10 +116,22 @@ fun parseFile(file: File): Int {
             e: RecognitionException?
         ) {
             hasErrors = true
-            val errMsg = "line $line:$charPositionInLine $msg"
+            val errMsg = "$line:$charPositionInLine $msg"
             println(errMsg)
         }
     })
-    parser.program()
-    return if (hasErrors) 1 else 0
+    val program = parser.program()
+    return if (hasErrors) Pair(1, program) else Pair(0, program)
+}
+
+fun makeIR(tree: DecafParser.ProgramContext): IRProgram = parseTreeToIR(tree) as IRProgram
+
+fun semanticCheck(program: IRProgram, debug: Boolean): Int {
+    val errors = checkSemantics(program)
+    if (debug) println(irToString(program, 0))
+    return if (errors.isEmpty()) 0
+    else {
+        errors.forEach { println(it) }
+        1
+    }
 }
