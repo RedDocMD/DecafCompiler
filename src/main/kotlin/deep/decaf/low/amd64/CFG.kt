82f25e28e98c7ce03af21220c038aebc18bdf7a4
Deep Majumder <deep.majumder2019@gmail.com>
2020-12-11 00:12:34 +0530

    Added JumpNode and DeclarationNode Completed construct CFG from block Completed break and continue statements

--- src/main/kotlin/deep/decaf/low/amd64/CFG.kt
+++ src/main/kotlin/deep/decaf/low/amd64/CFG.kt
@@ -26,6 +26,21 @@ class ConditionalNode(
     val condition: IRExpr
 ) : SingleInput
 
+sealed class JumpNodes(
+    override var next: CFGNode?,
+    override var prev: CFGNode?
+) : SingleInput, SingleOutput
+
+class BreakNode(next: CFGNode?, prev: CFGNode?) : JumpNodes(next, prev)
+class ContinueNode(next: CFGNode?, prev: CFGNode?) : JumpNodes(next, prev)
+class ReturnNode(next: CFGNode?, prev: CFGNode?) : JumpNodes(next, prev)
+
+class DeclarationNode(
+    override var next: CFGNode?,
+    override var prev: CFGNode?,
+    val declaration: IRMemberDeclaration
+) : SingleInput, SingleOutput
+
 class NoOpNode(
     var prevs: List<CFGNode>,
     override var next: CFGNode?
@@ -40,22 +55,46 @@ fun constructCFG(statement: IRStatement): CFG {
             node.statements.add(statement)
             CFG(node, node)
         }
-        is IRBreakStatement -> TODO()
-        is IRContinueStatement -> TODO()
+        is IRBreakStatement -> {
+            val node = BreakNode(null, null)
+            CFG(node, node)
+        }
+        is IRContinueStatement -> {
+            val node = ContinueNode(null, null)
+            CFG(node, node)
+        }
         is IRIfStatement -> {
             val ifCFG = constructCFG(statement.ifBlock)
-            if (statement.elseBlock != null) {
-                val elseCFG = constructCFG(statement.elseBlock)
-                val noOP = NoOpNode(listOf(ifCFG.exit, elseCFG.exit), null)
-                ifCFG.exit.next = noOP
-                elseCFG.exit.next = noOP
-                val branchNode = ConditionalNode(null, ifCFG.entry, elseCFG.exit, statement.condition)
-                CFG(branchNode, noOP)
+            val elseCFG = constructCFG(statement.elseBlock)
+            if (ifCFG != null && elseCFG != null) {
+                val noOp = NoOpNode(listOf(ifCFG.exit, elseCFG.exit), null)
+                ifCFG.exit.next = noOp
+                elseCFG.exit.next = noOp
+                val branchNode = ConditionalNode(
+                    null, ifCFG.entry, elseCFG.entry, statement.condition
+                )
+                CFG(branchNode, noOp)
+            } else if (ifCFG != null) {
+                val branchNode = ConditionalNode(
+                    null, ifCFG.entry, null, statement.condition
+                )
+                val noOp = NoOpNode(listOf(ifCFG.exit, branchNode), null)
+                branchNode.falsePath = noOp
+                CFG(branchNode, noOp)
+            } else if (elseCFG != null) {
+                val branchNode = ConditionalNode(
+                    null, null, elseCFG.entry, statement.condition
+                )
+                val noOp = NoOpNode(listOf(branchNode, elseCFG.exit), null)
+                branchNode.truePath = noOp
+                CFG(branchNode, noOp)
             } else {
-                val branchNode = ConditionalNode(null, ifCFG.entry, null, statement.condition)
-                val noOP = NoOpNode(listOf(ifCFG.exit, branchNode), null)
-                branchNode.falsePath = noOP
-                CFG(branchNode, noOP)
+                val branchNode = ConditionalNode(
+                    null, null, null, statement.condition)
+                val noOp = NoOpNode(listOf(branchNode, branchNode), null)
+                branchNode.falsePath = noOp
+                branchNode.truePath = noOp
+                CFG(branchNode, noOp)
             }
         }
         is IRForStatement -> TODO()
@@ -65,6 +104,44 @@ fun constructCFG(statement: IRStatement): CFG {
     }
 }
 
-fun constructCFG(block: IRBlock): CFG {
-    TODO()
+fun constructCFG(block: IRBlock?): CFG? {
+    when {
+        block == null -> return null
+        block.fieldDeclarations.isNotEmpty() -> {
+            val fields = block.fieldDeclarations
+            var node = DeclarationNode(null, null, fields[0])
+            val entry = node
+            var pt = node
+            for (field in fields.subList(1, fields.size)) {
+                node = DeclarationNode(null, pt, field)
+                pt.next = node
+                pt = node
+            }
+            var npt: SingleOutput = pt
+            for (statement in block.statements) {
+                val cfg = constructCFG(statement)
+                npt.next = cfg.entry
+                cfg.entry.prev = npt
+                npt = cfg.exit
+            }
+            return CFG(entry, npt)
+        }
+        block.statements.isNotEmpty() -> {
+            val statements = block.statements
+            var cfg = constructCFG(statements[0])
+            val entry = cfg.entry
+            var pt = cfg.exit
+            for (statement in statements.subList(1, statements.size)) {
+                cfg = constructCFG(statement)
+                pt.next = cfg.entry
+                cfg.entry.prev = pt
+                pt = cfg.exit
+            }
+            return CFG(entry, pt)
+        }
+        else -> {
+            return null
+        }
+    }
 }
+
