0cf2a7f1073f1ddf35ec1b1ddbf61d1ff02891be
Deep Majumder <deep.majumder2019@gmail.com>
2020-12-17 14:44:34 +0530

    Translated arithmetic expressions to LowIR with placeholders

--- src/main/kotlin/deep/decaf/low/amd64/LowIRClasses.kt
+++ src/main/kotlin/deep/decaf/low/amd64/LowIRClasses.kt
@@ -1,5 +1,10 @@
 package deep.decaf.low.amd64
 
+import deep.decaf.ir.*
+import java.util.*
+
+fun getUUID(): String = UUID.randomUUID().toString().replace("-", "")
+
 sealed class Offset
 data class NumberOffset(val offset: Int) : Offset()
 data class StringOffset(val name: String) : Offset()
@@ -13,19 +18,15 @@ data class Register(val name: String, val offset: Offset?) : Location() {
         fun stackPointer() = Register("rsp", null)
         fun instructionPointer() = Register("rip", null)
         fun returnRegister() = Register("rax", null)
+        fun reg10() = Register("r10", null)
+        fun reg11() = Register("r11", null)
+        fun regAX() = Register("rax", null)
+        fun regDX() = Register("rdx", null)
     }
 }
 
 data class Label(val label: String) : Location()
 data class MemLoc(val reg: Register, val offset: NumberOffset) : Location()
-
-enum class AsmArithOp {
-    ADD,
-    SUB,
-    IMUL,
-    IDIV,
-    CMP
-}
 
 enum class AsmCMoveOp {
     CMOVE,
@@ -43,14 +44,18 @@ enum class AsmJumpOp {
 }
 
 sealed class Statement
-data class ArithStatement(val op: AsmArithOp, val src: Location, val dest: Location) : Statement()
+data class AddStatement(val src: Location, val dest: Location) : Statement()
+data class SubStatement(val src: Location, val dest: Location) : Statement()
+data class IMulStatement(val src: Location, val dest: Location) : Statement()
+data class IDivStatement(val src: Location) : Statement()
 data class JumpStatement(val type: AsmJumpOp, val target: String) : Statement()
 data class MoveStatement(val src: Location, val dest: Location) : Statement()
 data class CMoveStatement(val type: AsmCMoveOp, val src: Register, val dest: Register) : Statement()
+object SignedExtendStatement : Statement()
 object ReturnStatement : Statement()
 data class CallStatement(val label: String) : Statement()
 data class PushStatement(val src: Location) : Statement()
-data class PopStatement(val src: Location) : Statement()
+data class PopStatement(val src: Location?) : Statement()
 object LeaveStatement : Statement()
 data class EnterStatement(val size: ImmediateVal) : Statement()
 
@@ -69,3 +74,78 @@ data class Program(
     var globalArrays: List<String>,
     var methods: List<Method>
 )
+
+fun irBinOpExprToLow(expr: IRBinOpExpr): List<Statement> {
+    val statements = mutableListOf<Statement>()
+
+    fun traverse(expr: IRExpr): Location? {
+        return when (expr) {
+            is IRIntLiteral -> ImmediateVal(expr.lit)
+            is IRBoolLiteral -> ImmediateVal(if (expr.lit) 1 else 0)
+            is IRMethodCallExpr -> {
+                val argLocations = expr.argList.map { traverse(it) }
+                argLocations.forEach { statements.add(PushStatement(it!!)) }
+                statements.add(CallStatement(expr.name))
+                for (i in 1..expr.argList.size) {
+                    statements.add(PopStatement(null))
+                }
+                Register.returnRegister()
+            }
+            is IRCallOutExpr -> TODO()
+            is IRBinOpExpr -> {
+                val leftLocation = traverse(expr.left)
+                val rightLocation = traverse(expr.right)
+                statements.add(MoveStatement(leftLocation!!, Register.reg10()))
+                statements.add(MoveStatement(rightLocation!!, Register.reg11()))
+                when (expr.op) {
+                    BinOp.ADD -> {
+                        statements.add(AddStatement(Register.reg11(), Register.reg10()))
+                        val tmp = Label(getUUID())
+                        statements.add(MoveStatement(Register.reg10(), tmp))
+                        tmp
+                    }
+                    BinOp.SUBTRACT -> {
+                        statements.add(SubStatement(Register.reg11(), Register.reg10()))
+                        val tmp = Label(getUUID())
+                        statements.add(MoveStatement(Register.reg10(), tmp))
+                        tmp
+                    }
+                    BinOp.MULTIPLY -> {
+                        statements.add(IMulStatement(Register.reg11(), Register.reg10()))
+                        val tmp = Label(getUUID())
+                        statements.add(MoveStatement(Register.reg10(), tmp))
+                        tmp
+                    }
+                    BinOp.DIVIDE -> {
+                        statements.add(MoveStatement(Register.reg10(), Register.regAX()))
+                        statements.add(SignedExtendStatement)
+                        statements.add(IDivStatement(Register.reg11()))
+                        val tmp = Label(getUUID())
+                        statements.add(MoveStatement(Register.regAX(), tmp))
+                        tmp
+                    }
+                    BinOp.REMAINDER -> {
+                        statements.add(MoveStatement(Register.reg10(), Register.regAX()))
+                        statements.add(SignedExtendStatement)
+                        statements.add(IDivStatement(Register.reg11()))
+                        val tmp = Label(getUUID())
+                        statements.add(MoveStatement(Register.regDX(), tmp))
+                        tmp
+                    }
+                    BinOp.LESS -> TODO()
+                    BinOp.MORE -> TODO()
+                    BinOp.LESS_OR_EQ -> TODO()
+                    BinOp.MORE_OR_EQ -> TODO()
+                    BinOp.EQ -> TODO()
+                    BinOp.NOT_EQ -> TODO()
+                    BinOp.AND -> TODO()
+                    BinOp.OR -> TODO()
+                }
+            }
+            is IRUnaryOpExpr -> TODO()
+            is IRLocationExpression -> TODO()
+        }
+    }
+
+    return statements
+}
